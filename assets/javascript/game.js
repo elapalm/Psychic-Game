620a14536c31cccb84a09f0dbcce1c23230f4948
emma lapalm <lillypadcrafts@gmail.com>
2019-02-14 17:57:38 -0700

    started.

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// the array to hold the letters
+
+var imThinking = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+
+// beginning counts
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letterUser = [];
+var eachofLetters = null;
+
+// the game
+
+var userGuess = imThinking[Math.floor(math.random() * imThinking.length)];
+
+function countGuessesLeft() {
+
+}
+
+    
