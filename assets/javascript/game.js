509aa8714c4500a0a07db2777a3452f3f42131e2
emma lapalm <lillypadcrafts@gmail.com>
2019-02-16 00:08:34 -0700

    now I've confused myself

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,6 @@
 
 var imThinking = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
 
-// possible choices
-
-var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
 
 // counts for wins, losses etc....
 
@@ -12,7 +9,7 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var letterUser = [];
-var eachOfLetters = null;
+var computerGuess = [];
 
 // refrence variables
 var computerChoice = document.getElementById("directions");
@@ -24,8 +21,33 @@ var lettersChosen = document.getElementById("current-letters");
 
 // the game
 document.onkeyup = function (event) {
-    var userGuess = event.key; 
-    var computerGuess = imThinking[Math.floor(Math.random() * imThinking.length)];
+    userGuess = event.key; 
+    computerGuess = imThinking[Math.floor(Math.random() * imThinking.length)];
+    computerChoice.push(imThinking);
+    console.log(computerChoice[0])
+
+    if ((userGuess === computerGuess[0]) && (guessesLeft >0)){
+        wins ++;
+        guessesLeft = 9;
+        lettersChosen.length = 0;
+        computerGuess.length = 0;
+        compGuess = imThinking[Math.floor(Math.random() * imThinking.length)];
+        console.log(computerChoice[0]);
+    }
+    else if ((userGuess !== computerChoice[0]) && (guessesLeft > 0)) {
+        losses -1;
+    }
+    else {
+        losses ++;
+        guessesLeft =9;
+        lettersChosen.length = 0;
+        compGuess = 0;
+        compGuess = imThinking[Math.floor(Math.random() * imThinking.length)];
+        computerGuess.push(compGuess);
+        console.log(compGuess)
+    }
+        
+
 } 
 
 
