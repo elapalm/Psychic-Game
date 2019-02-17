a2dffe843d747b110972cebb9ca97dfb33c944bb
emma lapalm <lillypadcrafts@gmail.com>
2019-02-16 18:06:29 -0700

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,13 @@
 // the array to hold the letters 
 
-var imThinking = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
-
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 // counts for wins, losses etc....
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var letterUser = [];
-var computerGuess = [];
+var userGuess = [];
 
 // refrence variables
 var computerChoice = document.getElementById("directions");
@@ -21,34 +19,41 @@ var lettersChosen = document.getElementById("current-letters");
 
 // the game
 document.onkeyup = function (event) {
-    userGuess = event.key; 
-    computerGuess = imThinking[Math.floor(Math.random() * imThinking.length)];
-    computerChoice.push(imThinking);
-    console.log(computerChoice[0])
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userGuess);
+    var userGuess = event.key;
+    var computerChoices = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+};
 
-    if ((userGuess === computerGuess[0]) && (guessesLeft >0)){
-        wins ++;
-        guessesLeft = 9;
-        lettersChosen.length = 0;
-        computerGuess.length = 0;
-        compGuess = imThinking[Math.floor(Math.random() * imThinking.length)];
-        console.log(computerChoice[0]);
-    }
-    else if ((userGuess !== computerChoice[0]) && (guessesLeft > 0)) {
-        losses -1;
-    }
-    else {
-        losses ++;
-        guessesLeft =9;
-        lettersChosen.length = 0;
-        compGuess = 0;
-        compGuess = imThinking[Math.floor(Math.random() * imThinking.length)];
-        computerGuess.push(compGuess);
-        console.log(compGuess)
-    }
-        
+// scoring
+if (userGuess.indexOf(userGuess) > -1) {
+    wins++;
+    guessesLeft = 9;
+    userGuess = [];
+};
+if (userGuess != computerGuess) {
+    guessesLeft - 1;
+    userGuess.push(userGuess);
+}
+if (userGuess === 0) {
+    losses++;
+    userGuess = [];
+};
 
-} 
+// displays the users guess, wins, losses, etc...
+var html =
+    currentGuesses.textContent = "You chose: " + userGuess;
+    correctGuess.textContent = "Wins: " + computerGuess;
+    wrongGuess.textContent = "Losses: " + losses;
+    remainingGuesses.textContent = "Guesses remaining: " + guessesLeft;
+    lettersChosen.textContent = "Letters Guessed: " + userGuess;
+
+    document.querySelector("#game").innerHTML = html;
+
+
+
+
 
 
 
